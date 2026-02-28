6c743fb1a92c10817bda4057b2bdcb2108dd94f3
Soha-elgammal <sohafathy226@gmail.com>
2026-03-01 00:02:38 +0200

    part 1

--- oop4/Program.cs
+++ oop4/Program.cs
@@ -1,5 +1,38 @@
 ﻿namespace oop4
 {
+    #region Part 1 : Theoretical Questions
+    
+    // Question 1
+    // Difference between Static Binding and Dynamic Binding
+    
+    // Static Binding: Happens at compile time.
+    // Example: Method Overloading.
+    //
+    // Dynamic Binding: Happens at runtime.
+    // Example: Method Overriding using virtual/override.
+
+    // Question 2
+    // Difference between Overloading and Overriding
+    
+    // Method Overloading:
+    // Same method name, different parameters.
+    // Happens in the same class.
+    // Compile-time polymorphism.
+    //
+    // Method Overriding:
+    // Same method name and same parameters.
+    // Happens between base and derived classes.
+    // Runtime polymorphism.
+
+   
+    // Question 3
+    // Keywords used for Method Overriding
+    
+    // virtual  -> Allows method to be overridden.
+    // override -> Provides new implementation in child class.
+    // base     -> Calls base class version of method.
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
